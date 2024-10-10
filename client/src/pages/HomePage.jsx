1bfb713b950c4ec23e02dd4bf592c8138b6d1514
Bhagya Yasarathna <bhagyayasarathna@gmail.com>
2024-10-10 20:52:51 +0530

    added Navbar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+};
 
-export default HomePage
+export default HomePage;
